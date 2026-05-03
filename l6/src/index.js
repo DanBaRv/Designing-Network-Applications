deb4117be0ba702f5d9d8151aebeecfdb5ce8d20
Dan <danya.b1@ya.ru>
2026-05-03 23:47:40 +0300

    Fullstack release: Backend + Vite Bundle

---
+++ l6/src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const rulesRouter = require("./routes/rules.js");
+const rulesService = require("./services/rulesFilterService.js");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+
+const DATA_FILE_PATH = path.join(__dirname, "data/rules.json");
+
+rulesService.init(DATA_FILE_PATH);
+app.use(cors()); 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+app.use("/rules", rulesRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Маршрут не найден" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Внутренняя ошибка сервера" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен по адресу http://localhost:${PORT}`);
+});
