d7e7c491f42f9cdee8d755653bc6c27ab084e053
Dan <danya.b1@ya.ru>
2026-05-28 23:23:15 +0300

    final update: Fetch API, async/await and Vite build integration

---
+++ l6/service/index.js
@@ -0,0 +1,69 @@
+import { BackButtonComponent } from "../../components/back-button/index.js";
+import { MainPage } from "../main/index.js";
+import { ServiceAccordionComponent } from "../../components/service-accordion/index.js";
+import { rulesUrls } from "../../modules/rulesUrls.js";
+import { ajax } from "../../modules/ajax.js";
+
+export class ServicePage {
+  constructor(parent, id) {
+    this.parent = parent;
+    this.id = id;
+  }
+
+  async getData() {
+    try {
+      const data = await ajax.get(rulesUrls.getRulesById(this.id));
+      this.renderData(data);
+    } catch (error) {
+      console.error("Ошибка при получении данных:", error);
+    }
+  }
+
+  getHTML() {
+    return `<div id="service-page" class="container py-4"></div>`;
+  }
+
+  get pageRoot() {
+    return document.getElementById("service-page");
+  }
+
+  clickBack() {
+    const mainPage = new MainPage(this.parent);
+    mainPage.render();
+  }
+  ////////////////////////
+  renderData(data) {
+    if (!data) return;
+    this.pageRoot.insertAdjacentHTML(
+      "beforeend",
+      `<h1 class="mt-4 mb-3">${data.title}</h1>`,
+    );
+
+    this.pageRoot.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="mb-4">
+            <img src="${data.src}" 
+                 class="img-fluid" 
+                 style="height: 500px; border-radius: 20px; object-fit: cover;" 
+                 alt="${data.title}">
+        </div>
+      `,
+    );
+    const accordion = new ServiceAccordionComponent(this.pageRoot);
+    accordion.render(data);
+  }
+
+  ////////////////////////
+
+  render() {
+    this.parent.innerHTML = "";
+
+    const html = this.getHTML();
+    this.parent.insertAdjacentHTML("beforeend", html);
+
+    const backButton = new BackButtonComponent(this.pageRoot);
+    backButton.render(this.clickBack.bind(this));
+    this.getData();
+  }
+}
